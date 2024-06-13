8270addcc0d2295145e503244835772e03458915
Dixit Sathwara <dixitgsathwara1708@gmail.com>
2024-06-13 16:41:04 +0530

    header_img updated

--- Frontend/src/components/Header/Header.jsx
+++ Frontend/src/components/Header/Header.jsx
@@ -7,7 +7,7 @@ export const Header = () => {
       <div className="header-contents">
         <h2>Order your Favourite Food</h2>
         <p>Choose form deriver menu featuring a delicated array of dishes crafted with the finest ingrediants and culinary experties. our mission is to satisfy your cravings and elevate your dining experiance</p>
-        <button>View Menu</button>
+        <a href="#explore-menu"><button>View Menu</button></a>
       </div>
     </div>
   )
